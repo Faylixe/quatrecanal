b9436a1eaef86a82501e2ce302894eb66369505a
Faylixe <felix.voituret@gmail.com>
2016-10-06 15:29:12 +0200

    Adding error message formatting and board request handling

--- quatrecanal.py
+++ quatrecanal.py
@@ -15,10 +15,22 @@ PRIORITY = ['.gif', '.webm', '.png', '.jpg']
 # Access token for slack integration.
 ACCESS_TOKEN = os.environ.get('SLACK_WEBHOOK_TOKEN')
 
+# Default board to use.
+DEFAULT_BOARD = 'gif'
+
+def getBoards():
+    """ Retrieves list of available boards. """
+    response = requests.get('http://a.4cdn.org/boards.json')
+    json = response.json()
+    boards = []
+    for board in json['boards']:
+        boards.append(board['board'])
+    return boards
+
 class Crawler:
     """ 4Chan crawler based on read only API. """
 
-    def __init__(self, board='b'): # TODO : Add filter.
+    def __init__(self, board): # TODO : Add filter.
         """ Default constructor. """
         self.board = board
         self.images = {}
@@ -46,30 +58,41 @@ class Crawler:
         """
         firstpage = self.getResource(1)
         if not 'threads' in firstpage:
-            return -1, 'No thread provided'
+            return -1, 'No thread found in board %s' % self.board
         self.parseImages(firstpage['threads'])
         for extension in PRIORITY:
             if extension in self.images:
                 selected = randint(0, len(self.images[extension]))
                 name = self.images[extension][selected]
                 return 0, 'http://i.4cdn.org/%s/%s%s' % (self.board, name, extension)
-        return -1, 'No valid resource found'
+        return -1, 'No valid image resource found'
 
 # Web service instance.
 service = Flask(__name__)
+
+# Valid board list
+boards = getBoards()
 
 @service.route('/', methods=['POST'])
 def webhook():
     """ Webhook implementation. """
     if request.form.get('token') == ACCESS_TOKEN:
+        board = request.form.get('text').replace(' ', '')
+        if len(board) == 0 or not board in boards:
+            board = DEFAULT_BOARD
         response = {}
         response['response_type'] = 'in_channel'
-        crawler = Crawler('gif')
-        result, url = crawler.getRandomImage()
-        # TODO : Handle error code.
-        response['text'] = url
+        crawler = Crawler(board)
+        result, data = crawler.getRandomImage()
+        if result == -1:
+            message = {}
+            message['color'] = '#DD2222'
+            message['text'] = 'Sorry an error occurs while retriving image : %s' % data
+            response['attachements'] = [message]
+        else:
+            response['text'] = data
         return Response(json.dumps(response), mimetype='application/json')
-    return Response('BIATCH'), 401
+    return Response(), 401
 
 if __name__ == '__main__':
     service.run(host='0.0.0.0')
